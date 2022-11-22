edfe16b485a5f589a82b53d0335644ea6bd2ba61
mohammedboreny <Mohammed.boreny@gmail.com>
2022-11-22 12:56:45 +0300

    Comments Updated

--- src/Components/AuthForms/ByGoogle.jsx
+++ src/Components/AuthForms/ByGoogle.jsx
@@ -5,41 +5,43 @@ import { GoogleLogin } from 'react-google-login';
 import { useAuth } from '../../CurrentUserContext';
 import { loadGapiInsideDOM } from "gapi-script";
 
-
+// Google auth 2.0 api key 
 const clientID = '1084259004154-f6s449bespfc5otl96o0vkr5qpd7p43j.apps.googleusercontent.com';
 
 function ByGoogle() {
-    const { loggedin,login,idSet } = useAuth();
-
+    const { loggedin, login, idSet } = useAuth();
+    // Success and Failure functions
     const onSuccess = (res) => {
         console.log('[Login Success] currentUser', res.profileObj)
         login();
-        
+
     }
     const onFailure = (res) => {
-        console.log('[Login failed] currentUser',res)
+        console.log('[Login failed] currentUser', res)
     }
 
+    //  to confirm certificate the connection with the google api
     useEffect(() => {
         (async () => {
-          await loadGapiInsideDOM();
+            await loadGapiInsideDOM();
         })();
-      });
+    });
 
-  return (
-      <div>
+    return (
+        <div>
+            {/* Google login Component attributes editing */}
             <GoogleLogin
-    clientId={clientID}
-    buttonText="Login"
-    onSuccess={onSuccess}
-    onFailure={onFailure}
-              cookiePolicy={'single_host_origin'}
-              isSignedIn={true}
-          />
-          
-          
-    </div>
-  )
+                clientId={clientID}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                isSignedIn={true}
+            />
+
+
+        </div>
+    )
 }
 
 export default ByGoogle
